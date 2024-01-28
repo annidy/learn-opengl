5d5a274ec56a5de6b01f66527f0dbd2116bcd2c1
annidy <annidy@gmail.com>
2024-01-28 17:24:21 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,168 @@
+#include <GL/glew.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <array>
+#include <iostream>
+
+
+constexpr auto vertexShaderSource = R"(
+    #version 330 core
+    
+    layout (location = 0) in vec4 aPos;
+
+    void main()
+    {
+        gl_Position = aPos;
+    }
+)";
+
+constexpr auto fragmentShaderSource = R"(
+    #version 330 core
+
+    layout(location = 0) out vec4 FragColor;
+
+    void main()
+    {
+        FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    }
+)";
+
+unsigned int CompileShader(const std::string &source, unsigned int type)
+{
+    unsigned int id = glCreateShader(type);
+    const char *src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char *message = (char *)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cerr << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+        std::cerr << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+    return id;
+}
+
+unsigned int CreateShaderProgram(const std::string &vertexShaderSource, const std::string &fragmentShaderSource)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(vertexShaderSource, GL_VERTEX_SHADER);
+    unsigned int fs = CompileShader(fragmentShaderSource, GL_FRAGMENT_SHADER);
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+    // Optional: Delete the shaders once linked
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    return program;
+}
+
+void framebufferSizeChanged(GLFWwindow *window, int width, int height);
+
+static void error_callback(int error, const char *description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+int main()
+{
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    
+    GLFWwindow *window = glfwCreateWindow(600, 480, "OpenGL + GLFW", NULL, NULL);
+    if (window == NULL)
+    {
+        const char *description;
+        int error = glfwGetError(&description);
+        if (error != GLFW_NO_ERROR)
+        {
+            fprintf(stderr, "GLFW Error: %s\n", description);
+        }
+
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    fprintf(stdout, "GLEW version: %s\n", glewGetString(GLEW_VERSION));
+    fprintf(stdout, "GL version: %s\n", glGetString(GL_VERSION));
+
+    // glfwSetFramebufferSizeCallback(window, framebufferSizeChanged);
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.0f, 0.5f};
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    unsigned int shaderProgram = CreateShaderProgram(vertexShaderSource, fragmentShaderSource);
+    glUseProgram(shaderProgram);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    // glDeleteVertexArrays(1, &VAO);
+    // glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebufferSizeChanged(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
